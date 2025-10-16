917ff6d7638bb354ec5f05ab8df17d2217e143e2
Mr-cody-77 <sonu77mahata@gmail.com>
2025-10-17 01:45:16 +0530

    first commit

--- backend/leaderboard/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Leaderboard
-from .models import Player
-# Register your models here.
-admin.site.register(Leaderboard)
-admin.site.register(Player)
